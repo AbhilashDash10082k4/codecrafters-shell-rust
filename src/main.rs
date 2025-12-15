7c47b5bc893749dc9448aa337f919b01c7ca657a
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2025-12-16 01:11:43 +0530

    REPL implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
 use std::io::{self, Write};
-use std::env;
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
@@ -11,15 +10,18 @@ fn main() {
     let command:Vec<String> = env::args().collect();
     dbg!(&command);
     env::args() = iterator, .collect() -turns the iterator into a collection
-    
+
     dbg- used with #[derive(Debug)] -used to print the types which do not implement Display trait by default and Debug(a trait) is used to print the all the values of the type -used with {:?} or {:#?} in println!()
 
     debug = macro, Debug = trait
 
     prints the line no. and the val where the dbg! is used and takes ownership of the val (println! takes reference) and returns it back to the expression*, else completely takes the ownership of the variable passed into it
     */
-    let mut command = String::new();
-    //taking the user i/p
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim())
+    loop {
+        let mut command = String::new();
+        //taking the user i/p
+        io::stdin().read_line(&mut command).unwrap();
+        //parsing the command
+        println!("{}: command not found", command.trim())
+    }
 }
