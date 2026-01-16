e72d152ee9cbf784eb05ab447072d9b2179a8cc2
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-01-17 01:48:44 +0530

    organised folder str

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -17,7 +17,7 @@ pub fn find_executable(cmd: &str) -> Option<PathBuf> {
             use std::os::unix::fs::PermissionsExt;
             if let Ok(metadata) = full_path.metadata() {
                 if metadata.permissions().mode() & 0o111 != 0 {
-                    println!("{command_to_be_printed} is {}", full_path.display());
+                    println!("{cmnd} is {}", full_path.display());
                     return Some(full_path);
                 }
             }
