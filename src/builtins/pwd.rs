d431309e797f0292c26df5ad694e09edc6be0e53
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-01-19 18:00:08 +0530

    stage 11 -pwd command

--- src/builtins/pwd.rs
+++ src/builtins/pwd.rs
@@ -1,19 +1,17 @@
 use crate::commands::command::UserInput;
-use std::{env};
+use std::env;
 /*print the current directory
 type the pwd cmnd in shell -give it to OS -print the current dir
 */
-pub fn handle(cmnd: &UserInput) -> bool{
+pub fn handle(cmnd: &UserInput) -> bool {
     //i/p taken in form of argument
     let user_ip = cmnd.raw.trim();
     //match command with pwd and then send this to OS to fetch the curr dir
     if user_ip == "pwd" {
-        let Ok(curr_dir) = env::current_dir() else {
-            return false;
-        };
-        println!("{}", curr_dir.display());
+        if let Ok(curr_dir) = env::current_dir() {
+            println!("{}", curr_dir.display());
+        }
         return true;
     }
-        return false;
-    
+    false
 }
