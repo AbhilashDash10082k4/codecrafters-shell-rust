9092721ea9d434560c8f664fbebdf70ecd9a2dc5
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-01-24 16:40:26 +0530

    stage22

--- src/utils/childprocess_execution.rs
+++ src/utils/childprocess_execution.rs
@@ -1,8 +1,40 @@
-use std::{path::Path, process::{Command, Stdio}};
+use std::{
+    path::PathBuf,
+    process::{Command, Stdio},
+};
+/*Work of this block -take an executable file and handles it to the terminal -basically start a program
+- Command::new -prepares the file to be executed , takes in the name of the program and not the entire file path
+- args -takes the extra arguments provided by the user
+- .stdin(Stdio::inherit) -is a child program and takes i/p from keyboard as same as shell
+Diff b.w child program and shell-
+Shell -parent, is this current program and manager of diff OS processes, runs other codes(child process), takes the keyboard i/p
+Child program -ran by shell, temporary, exist only when the code is running, executed by OS, is the process created by OS to run a code
+stdin (i/p from keyboard), stdout (o/p to screen), stderr(err to screen) -pipes
+.Stdio::inherit() -makes the shell and child program share the same terminal/ o/p screen
+Shell controls who runs and child program controls what runs
+Command -tool to run other programs, used by Rust to command directly to OS
+.spawn -finally runs the program
+child.wait -used to hold the shell untill the program stops
+program_name != find_executable(program_name)
+*/
+/* let mut child = match Command::new(program_name)
+    .args(user_args)
+    .stdin(Stdio::inherit())
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()
+{
+    Ok(c) => c,
+    Err(e) => {
+        eprintln!("Error executing {program_name}: {e}");
+        return false;
+    }
+}; moved into child childprocess_execution::handle(&cmnd_arr);*/
 
-pub fn handle(cmnd: &Vec<String>) {
-    if let Ok(mut child) = Command::new(Path::new(&cmnd[0]))
-        .args(&cmnd[1..])
+pub fn handle(p: PathBuf, args: &Vec<String>) {
+    /*earlier &cmnd[0] worked as the executables didnt have spaces. But now, absolute paths are needed to pass inorder to make the execution successful */
+    if let Ok(mut child) = Command::new(p)
+        .args(&args[1..])
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
