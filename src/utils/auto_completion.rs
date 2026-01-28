35fa0f9c3bab9bd84505d9b6fb006bee37b5e9be
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-01-28 23:12:49 +0530

    stage27 - TAB autocompletion

--- src/utils/auto_completion.rs
+++ src/utils/auto_completion.rs
@@ -12,6 +12,7 @@ pub fn handle(args: &str) -> bool{
     if args.ends_with("\t") {
         for c in builtins {
             if c.starts_with(prefix) {
+                /*this does not overwrites the "ech" or "exi" */
                 print!("\r$ {c} ");
                 io::stdout().flush().unwrap();
                 return true;
