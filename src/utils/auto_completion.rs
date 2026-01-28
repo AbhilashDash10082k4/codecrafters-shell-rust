cc7890ac2950a20d3441a2d71cbcb17e081034b8
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-01-28 23:13:31 +0530

    stage27 - TAB autocompletion

--- src/utils/auto_completion.rs
+++ src/utils/auto_completion.rs
@@ -9,15 +9,19 @@ pub fn handle(args: &str) -> bool{
     let prefix = args
         .trim_end_matches('\n')
         .trim_end_matches('\t');
-    if args.ends_with("\t") {
+    if !args.ends_with('\t') {
+        return false;
+    }
         for c in builtins {
             if c.starts_with(prefix) {
-                /*this does not overwrites the "ech" or "exi" */
+                /*this does not overwrites the "ech" or "exi"
+                todo- take cursor to start -> overwrite old i/p (ech, exi) -> write teh completed o/p*/
                 print!("\r$ {c} ");
+                /*flush -> stateful -> sends bytes to terminal -> depends on cursor position*/
                 io::stdout().flush().unwrap();
                 return true;
             }
-        }
+        
     }
     false
 }
