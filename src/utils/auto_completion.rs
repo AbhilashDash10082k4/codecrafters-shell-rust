499b9ce11d903e3c48b7fbb4f68b4548d20f04f5
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-02-15 18:27:52 +0530

    stage30- LCP implementation

--- src/utils/auto_completion.rs
+++ src/utils/auto_completion.rs
@@ -236,7 +236,7 @@ fn autocomplete(prefix: &str, tab_cnt: &Cell<usize>, matches: Vec<&str>) -> Vec<
             io::stdout().flush().unwrap()
          } else if tab_cnt.get() == 2 {
             println!("\n{}", matches.join("  "));
-            println!("{}", prefix);
+            println!("$ {}", prefix);
             tab_cnt.set(0);
          }
       }
