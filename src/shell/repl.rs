7a073ee736ae66359fbb723ca08156f8077d408b
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-05-25 11:30:31 +0000

    pipeline for builtins

--- src/shell/repl.rs
+++ src/shell/repl.rs
@@ -1,7 +1,7 @@
 use crate::{
    builtins::{cd, echo, exit, pwd, type_cmd},
    commands::command::UserInput,
-   utils::{cmnd_parser, completion::tab_completion::TabCompleter, execute_file},
+   utils::{cmnd_parser, completion::tab_completion::TabCompleter, dual_cmmnd_pipeline::pipeline, execute_file},
 };
 use rustyline::{
    Config, Editor,
@@ -78,6 +78,7 @@ pub fn start() {
       if args.is_empty() {
          continue;
       }
+      pipeline::handle(&args);
       //parsing the UserInput
       if exit::handle(&args) {
          break;
